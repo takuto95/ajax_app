dbb435539de059de89db35b331cd4013046c2685
takuto95 <kaza.solution95@gmail.com>
2020-11-10 22:26:22 +0900

    メモ投稿機能のサーバサイド実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,8 +4,8 @@ class PostsController < ApplicationController
   end
 
   def create
-    Post.create(content: params[:content])
-    redirect_to action: :index
+    post = Post.create(content: params[:content],checked: false)
+    render json: { post: post }
   end
 
   def checked
@@ -18,6 +18,7 @@ class PostsController < ApplicationController
 
   item = Post.find(params[:id])
   render json: { post: item }
-end
+  end
+    
 
 end
